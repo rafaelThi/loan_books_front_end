98135e720b0ffdf00d902dbceffbaa92b11a9c60
Rafael Thiago Pereira de Melo <rafael.pereira20@icloud.com>
2021-01-05 17:01:42 -0300

    Criando rotas e a HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import Routes from './routes/routes';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Front loan books
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+   
   );
 }
 
---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return(
+    <>
+    <div>
+      <h1>Olá mundo</h1>
+    </div>
+    </>
+  )
+}
+export default HomePage;
