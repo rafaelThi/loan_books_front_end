b132686bcc8e0329843621f99747640eb97db16c
Rafael Thiago Pereira de Melo <rafael.pereira20@icloud.com>
2021-01-13 16:06:13 -0300

    mostrando alerta em tela dos erros

--- src/pages/AdminLogin/index.tsx
+++ src/pages/AdminLogin/index.tsx
@@ -28,9 +28,7 @@ const AdminPageLogin:React.FC = () => {
           ),
         password: Yup.string().min(6, 'Senha invalida, no minino 6 caracteres'),
       });
-      await schema.validate({ email, password }, {
-        abortEarly: false,
-      });
+      await schema.validate({ email, password });
       history.push('/');// registro de livros
     } catch (err) {
       alert('E-mail ou senha incorreto');
